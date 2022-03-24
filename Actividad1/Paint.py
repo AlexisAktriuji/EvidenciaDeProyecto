ac38193261ad59b29b3bdc25ed2b5addded6bbb8
A01750478@itesm.mx <A01750478@itesm.mx>
2022-03-24 09:15:28 -0600

    Hice la funcion del Triangulo

--- Actividad1/Paint.py
+++ Actividad1/Paint.py
@@ -38,8 +38,11 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    turtle.circle(80, 
-              extent = 180)
+    t = turtle.Turtle()
+  
+  
+    r = 50
+    t.circle(r)
 
 
 def rectangle(start, end):
@@ -49,7 +52,30 @@ def rectangle(start, end):
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    pass  # TODO
+    import turtle
+  
+    # creating turtle pen
+    t = turtle.Turtle()
+    
+    # taking input for the side of the triangle
+    s = int(input("Enter the length of the side of the triangle: "))
+    
+    # taking the input for the color
+    col = input("Enter the color name or hex value of color(# RRGGBB): ")
+    
+    # set the fillcolor
+    t.fillcolor(col)
+    
+    # start the filling color
+    t.begin_fill()
+    
+    # drawing the triangle of side s
+    for _ in range(3):
+        t.forward(s)
+        t.right(-120)
+    
+    # ending the filling of the color
+    t.end_fill()
 
 
 def tap(x, y):
