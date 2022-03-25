df3e4d81ad3308ba7418492c4c9ee46fb60d7d3b
A01749448 <a01749448@itesm.mx>
2022-03-25 00:23:44 -0600

    Agregué la funcionalidad que logra que los fantasmas sean más inteligentes y sigan a pacman

--- Actividad3/packman.py
+++ Actividad3/packman.py
@@ -8,10 +8,10 @@ writer = Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+    [vector(-180, 160), vector(10, 0)],
+    [vector(-180, -160), vector(0, 10)],
+    [vector(100, 160), vector(0, -10)],
+    [vector(100, -160), vector(-10, 0)],
 ]
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -115,12 +115,35 @@ def move():
         if valid(point + course):
             point.move(course)
         else:
-            options = [
-                vector(5, 0),
-                vector(-5, 0),
-                vector(0, 5),
-                vector(0, -5),
-            ]
+            # Lograr que los fantasmas sean más inteligentes y puedan seguir a pacman
+            if(pacman.x > point.x) and (pacman.y > point.y):
+                options = [
+                    vector(10, 0),
+                    vector(0, 10),
+                ]
+            elif(pacman.x < point.x) and (pacman.y < point.y):
+                options = [
+                    vector(-10, 0),
+                    vector(0, -10),
+                ]
+            elif(pacman.x > point.x) and (pacman.y < point.y):
+                options = [
+                    vector(10, 0),
+                    vector(0, -10),
+                ]
+            elif(pacman.x < point.x) and (pacman.y > point.y):
+                options = [
+                    vector(-10, 0),
+                    vector(0, 10),
+                ]
+            else:
+                options = [
+                    vector(5, 0),
+                    vector(-5, 0),
+                    vector(0, 5),
+                    vector(0, -5),
+                ]
+
             plan = choice(options)
             course.x = plan.x
             course.y = plan.y
